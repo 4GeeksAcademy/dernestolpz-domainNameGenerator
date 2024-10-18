daaf66f706c639b4ad1e5c944ddbb3e309ebc41c
dernestolpz <dernesto.lopez@gmail.com>
2024-10-18 19:26:25 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const extensions = [".com", ".net", ".org", ".io"];
+
+  const generateDomainNames = () => {
+    const domainNames = [];
+
+    for (let pronouns of pronoun) {
+      for (let adjs of adj) {
+        for (let nouns of noun) {
+          for (let extension of extensions) {
+            const domainName = `${pronouns}${adjs}${nouns}${extension}`;
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+
+    return domainNames;
+  };
+
+  const generatedDomains = generateDomainNames();
+  generatedDomains.forEach(domain => console.log(domain));
 };
